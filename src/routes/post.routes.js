6ac098a9937de575fb9c33e74a0a4596411dd1f2
Ana Rius <a.riusvi11@gmail.com>
2024-03-08 20:07:20 +0100

    feat: post routers created todo

---
+++ src/routes/post.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+
+
+const router = Router();
+// router.post('/', createPost) //todo crear post
+// router.delete('/:id' , deletePostById) //todo eliminar post por id
+// router.put('/', updatePost) //todo update post por id
+// router.get('/own', getOwnPost) //todo recuperar mis propios posts
+// router.get('/', getPost) //todo recuperar todos los posts
+// router.get('/:id', getPostById) //todo recuperar post por id
+// router.get('/users/posts/:user-id', getPostByUserId) //todo recuperar posts de un usuario por su id
+
+
+
+export default router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,8 +5,8 @@ import { getUsers } from "../controllers/user.controller.js";
 const router = Router();
 
 router.get('/', getUsers) //todo faltaría crear el filtro que solo pueda recuperar el super_admin
-// router.get ('/profile' , userProfile) //todo
-// router.put('/profile', updateUsersProfile) //todo
+// router.get ('/profile' , userProfile) //todo perfil de usuario
+// router.put('/profile', updateUsersProfile) //todo modificar datos del perfil
 
 
 export default router;
