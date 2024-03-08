3fd2913085969c5e96223c2fdbb7aefd5132f3e0
Ana Rius <a.riusvi11@gmail.com>
2024-03-08 21:50:41 +0100

    feat: auth middleware created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { getUsers } from "../controllers/user.controller.js";
 import { getUserProfile } from "../controllers/user.controller.js";
+import { auth } from "../middleware/auth.middleware.js";
 
 
 const router = Router();
 
-router.get('/', getUsers) //todo faltaría crear el filtro que solo pueda recuperar el super_admin
-router.get ('/profile', getUserProfile) //todo perfil de usuario
+router.get('/', auth, getUsers) //todo faltaría crear el filtro que solo pueda recuperar el super_admin
+router.get ('/profile', auth, getUserProfile) //todo perfil de usuario
 // router.put('/profile', updateUsersProfile) //todo modificar datos del perfil
 
 
