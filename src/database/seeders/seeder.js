dd91037e2f5b0e7d28419f1019e9539fc10c5c45
Ana Rius <a.riusvi11@gmail.com>
2024-03-11 20:53:18 +0100

    feat: post feeder created

--- src/database/seeders/seeder.js
+++ src/database/seeders/seeder.js
@@ -1,13 +1,15 @@
 import { dbConnection } from "../db.js";
 import mongoose from "mongoose";
-import userSeeder from "./userSeeder.js"
+// import userSeeder from "./userSeeder.js"
 import "dotenv/config";
+import seedPosts from "./postSeeder.js";
 
 const seed = async () => {
     try {
         await dbConnection();
         console.log("Database connected");
-        await userSeeder();
+        await seedPosts ();
+        // await userSeeder();
 
     } catch (error) {
         console.log("Database connection failed");
