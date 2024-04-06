65bf5d96bc85a3f52f8a4d4d01f168583d557723
Ana Rius <a.riusvi11@gmail.com>
2024-04-06 10:28:34 +0200

    feat: add cors package to package.json and import cors in server.js

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ import express from "express";
 import 'dotenv/config'
 import { dbConnection } from "./database/db.js";
 import router from './routes/router.js'
+import cors from 'cors'
 
 const app = express()
 
 // parsea el body
 app.use(express.json())
+app.use(cors);
 
 const PORT = process.env.PORT || 4001;
 
